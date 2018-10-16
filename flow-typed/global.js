0e0f2df86a98d123d68b70303e70d64d5c93cb79
Yohanes Bandung <bandungpenting@gmail.com>
2018-10-16 21:10:07 +0700

    flow-typed and installed

--- flow-typed/global.js
+++ flow-typed/global.js
@@ -1,7 +1,10 @@
 // @flow
 
 import type {Styles} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+import type {StyleObj} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 
-declare type StyleSet = number | Styles | Array<StyleSet>;
+declare type StyleSet = number | Styles | StyleObj | Array<StyleSet>;
+
+declare type ReactNode = React$Node;
 
 declare type ObjectOf<T> = {[string]: T};
