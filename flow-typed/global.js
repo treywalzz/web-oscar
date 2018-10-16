f9ffbcf316c33ba1d4d5971a6864f8ffddb63e3f
Yohanes Bandung <bandungpenting@gmail.com>
2018-10-16 11:22:08 +0700

    first initialization

---
+++ flow-typed/global.js
@@ -0,0 +1,7 @@
+// @flow
+
+import type {Styles} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
+
+declare type StyleSet = number | Styles | Array<StyleSet>;
+
+declare type ObjectOf<T> = {[string]: T};
--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-class App extends Component {
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default class App extends React.Component<{}> {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <View style={styles.container}>
+        <Text>Open up src/App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
     );
   }
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// class App extends Component<{}> {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
